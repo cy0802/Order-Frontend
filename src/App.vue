654d30eddefcd87851b2fa2e8706e4f41bd32e88
cy0802 <amyyoyo0802@gmail.com>
2024-09-29 00:09:49 +0800

    login and user data

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,12 @@
   <v-app>
     <v-app-bar app color="primary">
       <v-app-bar-title>王品集團</v-app-bar-title>
+      <v-spacer></v-spacer>
+      <LoginDialog 
+        @login="login"
+        v-if="!loggedIn"
+      />
+      <p v-if="loggedIn" class="mr-7">Hi, {{ userName }}</p>
     </v-app-bar>
     <v-main>
       <v-container>
@@ -13,13 +19,54 @@
 
 <script>
 import ProductTabs from './components/ProductTabs.vue';
+import LoginDialog from './components/LoginDialog.vue';
+import axios from 'axios';
 export default {
   name: 'App',
   components: {
-    ProductTabs
+    ProductTabs,
+    LoginDialog
+  },
+  data() {
+    return {
+      loggedIn: false,
+      userName: null,
+      userId: null,
+      userPhone: null,
+      userEmail: null,
+      accessToken: null,
+      admin: null
+    };
+  },
+  methods: {
+    async login(email, password) {
+      console.log(email, password);
+      try {
+        const response = await axios.post('http://localhost:8000/api/login', {
+          email: email,
+          password: password
+        });
+        const { id, name, email: userEmail, token, phone, admin } = response.data;
+        this.userId = id;
+        this.userName = name;
+        this.userEmail = userEmail;
+        this.userPhone = phone;
+        this.accessToken = token;
+        this.admin = admin;
+        this.loggedIn = true;
+      } catch (error) {
+        console.error(error);
+      }
+    },
+    logout() {
+      this.userId = null;
+      this.userName = null;
+      this.userPhone = null;
+      this.userEmail = null;
+      this.admin = null;
+      this.loggedIn = false;
+      this.accessToken = null;
+    }
   }
 }
 </script>
-
-<style>
-</style>
---
+++ src/components/LoginDialog.vue
@@ -0,0 +1,69 @@
+<template>
+  <v-btn 
+    icon
+    class="ml-7"
+    @click='dialog = true'
+  >
+    <v-icon>mdi-login</v-icon>
+  </v-btn>
+  <v-dialog v-model="dialog" max-width="500px">
+    <v-card>
+      <v-card-text class="mt-4">
+        <v-form ref="form" v-model="valid">
+          <v-text-field
+            v-model="email"
+            label="Email"
+            :rules="emailRules"
+            required
+            variant="outlined"
+            class="mb-2"
+          ></v-text-field>
+          <v-text-field
+            v-model="password"
+            label="Password"
+            :rules="passwordRules"
+            type="password"
+            required
+            variant="outlined"
+          ></v-text-field>
+        </v-form>
+      </v-card-text>
+      <v-card-actions class='mb-2 mr-4'>
+        <v-btn color="error" variant="tonal" text @click="dialog = false">取消</v-btn>
+        <v-btn color="success" variant="tonal" text @click="login">登入</v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-dialog>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      dialog: false,
+      valid: false,
+      email: '',
+      password: '',
+      emailRules: [
+        v => !!v || 'Email is required',
+        v => /.+@.+\..+/.test(v) || 'Email must be valid',
+      ],
+      passwordRules: [
+        v => !!v || 'Password is required',
+        v => v.length >= 6 || 'Password must be at least 6 characters',
+      ],
+    };
+  },
+  methods: {
+    login() {
+      if (this.$refs.form.validate()) {
+        this.$emit('login', this.email, this.password );
+        this.dialog = false;
+      }
+    },
+  },
+};
+</script>
+
+<style scoped>
+</style>
